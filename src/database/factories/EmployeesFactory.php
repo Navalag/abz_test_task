c30b1b5b73fa379be54522cf73dfbdc18973d925
Andrew <agalavan@student.unit.ua>
2019-01-30 13:18:24 +0200

    create employees table, fill it with dummy data

--- src/database/factories/EmployeesFactory.php
+++ src/database/factories/EmployeesFactory.php
@@ -6,8 +6,8 @@ $factory->define(App\Employee::class, function (Faker $faker) {
     return [
         'fio' => $faker->name,
         'position' => $faker->jobTitle,
-        'employment_date' => now(),
-        'salary' => 1500,
-        'boss_id' => str_random(10),
+        'employment_date' => date("Y-m-d H:i:s",rand(1262055681,time())),
+        'salary' => random_int(1000, 5000),
+        'boss_id' => random_int(0, 9),
     ];
 });
--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-// use Faker\Generator as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,16 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	$faker = Faker\Factory::create();
-
-    	for($i = 0; $i < 1000; $i++) {
-	        App\Employee::create([
-	            'fio' => $faker->name,
-                'position' => $faker->jobTitle,
-                'employment_date' => date("Y-m-d H:i:s",rand(1262055681,time())),
-                'salary' => random_int(1000, 5000),
-                'boss_id' => random_int(0, 9),
-	        ]);
-	    }
+    	$empoyees = factory(App\Employee::class, 1000)->create();
     }
 }
